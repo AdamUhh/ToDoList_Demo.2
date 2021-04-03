203dc95721ab8072aac8233d91b112038c7c45f4
AdamUhh <adam.okay@yahoo.com>
2021-04-03 17:33:42 +0400

    Added loading screen

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ auth.onAuthStateChanged((user) => {
         document.querySelector(".ACCOUNT__header_wrapper span").textContent = "Currently Logged in as: " + user.email;
     } else {
         //if user is null
-        console.log("logged out");
+        // console.log("logged out");
 
         //if user logged out, redirect to auth.html
         history.pushState("auth.html", "", "auth.html");
@@ -88,6 +88,9 @@ function getFSData(userUIDRef) {
 }
 
 function CreateElements() {
+    // remove loading screen
+    document.querySelector("#loading").remove();
+
     if (Object.entries(groupDict).length != 0) {
         let Main_Padding = document.querySelector(".MAIN__padding");
         document.querySelector(".GROUP__ask_add_group_container").style.display = "none";
@@ -99,5 +102,7 @@ function CreateElements() {
         let menu_list = document.querySelector(".menu_list");
         let li = menu_list.querySelector("li");
         li.querySelector("a").click();
+    } else { // if there is no group
+        document.querySelector(".GROUP__ask_add_group_container").style.display = "block";
     }
 }
